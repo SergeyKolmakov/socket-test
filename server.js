4072645a5e2ea1968984ce7f8f3ba1b9c4f03ead
Kolmakov Sergey <s-consul@mail.ru>
2021-03-20 17:15:06 +0300

    Fix starting bugs

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 const port = 8080;
-const host = '0.0.0.0';
 
 app.use(express.static('public'));
 
@@ -14,12 +14,15 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', socket => {
-  socket.broadcast.emit('hello', 'world');
+  io.sockets.emit('broadcast', Date.now());
 
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('hello', 'googbye');
+  console.log('Отправлено уведомление всем');
+
+  io.on('disconnect', () => {
+    io.sockets.emit('broadcast', 'close');
   });
 });
 
-app.listen(port, host);
-// console.log(`running on http://${host}:${port}`);
+http.listen(port, () => {
+  console.log('listening on *:8080');
+});
