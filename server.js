e4242871fac6e4c6389574f03b3c2d72eba97370
Kolmakov Sergey <s-consul@mail.ru>
2021-03-20 20:29:10 +0300

    Add https and CORS

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 'use strict';
 
+const fs = require('fs');
+const cors = require('cors');
+const https = require('https');
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+// const http = require('http').Server(app);
+// const io = require('socket.io')(https);
 
 const port = 8080;
+const ip = require('ip').address();
 
+const options = {
+  key: fs.readFileSync('./file.pem'),
+  cert: fs.readFileSync('./file.crt'),
+};
+const serverPort = 443;
+
+const server = https.createServer(options, app);
+const io = require('socket.io')(server);
+
+app.use(cors());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+  console.log('Listening...');
 });
 
 io.on('connection', socket => {
@@ -23,6 +38,6 @@ io.on('connection', socket => {
   });
 });
 
-http.listen(port, () => {
-  console.log('listening on *:8080');
+server.listen(serverPort, function() {
+  console.log('server up and running at %s port', serverPort);
 });
